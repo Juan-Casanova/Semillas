333a4fbe085e23ba9495627f5c23d99f1c4ca18f
JuanCasnova <casanovajm199621@gmail.com>
2018-06-25 12:00:15 -0500

    primer comit sube los arquivos principales con composicion principal

---
+++ Carpeta/main.js
@@ -0,0 +1,15 @@
+//Definir pincel
+var canvas =document.getElementById("canvas");
+var ctx=canvas.getContext("2d");
+
+//Variables Constantes
+
+//Funciones
+
+//instancias
+
+//Mains
+
+//Funciones auxiliares
+
+//escuchadores
